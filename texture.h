05d10cfbc6db5a39b6eb1d7d946e40d67c9515e8
Gillett Hernandez <gillett.hernandez@gmail.com>
2020-02-28 14:04:25 -0800

    change texture.h::checker_texture to properly apply scale. but still exists though

--- texture.h
+++ texture.h
@@ -43,7 +43,7 @@ public:
     }
     virtual vec3 value(float u, float v, const vec3 &p) const
     {
-        float sines = scale * sin(10 * p.x()) * sin(10 * p.y()) * sin(10 * p.z());
+        float sines = sin(scale * p.x()) * sin(scale * p.y()) * sin(scale * p.z());
         if (sines < 0)
         {
             return odd->value(u, v, p);
